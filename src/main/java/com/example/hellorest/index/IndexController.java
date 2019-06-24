50b4d69c5c684b6ced8ed89c4dc803a578b29a68
moricom <moricom@injeinc.co.kr>
2019-06-24 18:01:23 +0900

    modify

--- src/main/java/com/example/hellorest/index/IndexController.java
+++ src/main/java/com/example/hellorest/index/IndexController.java
@@ -1,19 +1,21 @@
 package com.example.hellorest.index;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-//@RestController
+@RestController
 @Controller
 public class IndexController {
 	
-	@RequestMapping("/")
-	public String index() {
-		return "index.html";
-	}
-	
-//	@GetMapping("/")
+//	@RequestMapping("/")
 //	public String index() {
-//		return "Hello, Boot!";
+//		return "index.html";
 //	}
+	
+	@GetMapping("/")
+	public String index() {
+		return "Hello, Boot!";
+	}
 }

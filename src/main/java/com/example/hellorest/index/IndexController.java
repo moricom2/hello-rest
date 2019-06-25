9f3dfd83106e5ef0178d602b998f5a0e8c5843b3
moricom <moricom@injeinc.co.kr>
2019-06-25 14:58:54 +0900

    인덱스페이지수정

--- src/main/java/com/example/hellorest/index/IndexController.java
+++ src/main/java/com/example/hellorest/index/IndexController.java
@@ -5,17 +5,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-//@RestController
-@Controller
+@RestController
+//@Controller
 public class IndexController {
 	
-	@RequestMapping("/")
-	public String index() {
-		return "index.html";
-	}
-	
-//	@GetMapping("/")
+//	@RequestMapping("/")
 //	public String index() {
-//		return "Hello, Boot!";
+//		return "index.html";
 //	}
+	
+	@GetMapping("/")
+	public String index() {
+		return "Hello, Boot!";
+	}
 }
